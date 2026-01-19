4fb7b693ce0de85df701f1425a108f334df845e3
aliaaznl <imanalia06@gmail.com>
2026-01-19 10:22:27 +0800

    Rename 'color' field to 'colour' in Category entity

--- src/main/java/com/tracker/expensetracker/entity/Category.java
+++ src/main/java/com/tracker/expensetracker/entity/Category.java
@@ -12,7 +12,7 @@ public class Category {
     private Long id;
 
     private String name;
-    private String color; 
+    private String colour; 
     private String icon;
     
     // Many categories belong to One User (nullable for system categories)
